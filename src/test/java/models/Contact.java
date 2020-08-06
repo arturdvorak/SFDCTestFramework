4afab750c0366a5801bc2c767e3e26a838a356fe
Artur Dvorak <artur.dvorak@gmail.com>
2020-08-06 14:46:35 -0400

    lokbok usage

--- src/test/java/models/Contact.java
+++ src/test/java/models/Contact.java
@@ -1,187 +1,25 @@
 package models;
 
+import lombok.*;
+
+@Getter @Setter @AllArgsConstructor @Builder
 public class Contact {
-    String salutation;
-    String firstName;
-    String middleName;
-    String lastName;
-    String suffix;
-    String accountName;
-    String title;
-    String email;
-    String phone;
-    String mobile;
-    String reportsTo;
-    String department;
-    String fax;
-    String mailingStreet;
-    String mailingCity;
-    String mailingState;
-    String mailingZip;
-    String mailingCountry;
-
-    public Contact(String salutation, String firstName, String middleName, String lastName, String suffix, String accountName, String title, String email, String phone, String mobile, String reportsTo, String department, String fax, String mailingStreet, String mailingCity, String mailingState, String mailingZip, String mailingCountry) {
-        this.salutation = salutation;
-        this.firstName = firstName;
-        this.middleName = middleName;
-        this.lastName = lastName;
-        this.suffix = suffix;
-        this.accountName = accountName;
-        this.title = title;
-        this.email = email;
-        this.phone = phone;
-        this.mobile = mobile;
-        this.reportsTo = reportsTo;
-        this.department = department;
-        this.fax = fax;
-        this.mailingStreet = mailingStreet;
-        this.mailingCity = mailingCity;
-        this.mailingState = mailingState;
-        this.mailingZip = mailingZip;
-        this.mailingCountry = mailingCountry;
-    }
-
-    public String getSalutation() {
-        return salutation;
-    }
-
-    public void setSalutation(String salutation) {
-        this.salutation = salutation;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getMiddleName() {
-        return middleName;
-    }
-
-    public void setMiddleName(String middleName) {
-        this.middleName = middleName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getSuffix() {
-        return suffix;
-    }
-
-    public void setSuffix(String suffix) {
-        this.suffix = suffix;
-    }
-
-    public String getAccountName() {
-        return accountName;
-    }
-
-    public void setAccountName(String accountName) {
-        this.accountName = accountName;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getMobile() {
-        return mobile;
-    }
-
-    public void setMobile(String mobile) {
-        this.mobile = mobile;
-    }
-
-    public String getReportsTo() {
-        return reportsTo;
-    }
-
-    public void setReportsTo(String reportsTo) {
-        this.reportsTo = reportsTo;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-
-    public String getFax() {
-        return fax;
-    }
-
-    public void setFax(String fax) {
-        this.fax = fax;
-    }
-
-    public String getMailingStreet() {
-        return mailingStreet;
-    }
-
-    public void setMailingStreet(String mailingStreet) {
-        this.mailingStreet = mailingStreet;
-    }
-
-    public String getMailingCity() {
-        return mailingCity;
-    }
-
-    public void setMailingCity(String mailingCity) {
-        this.mailingCity = mailingCity;
-    }
-
-    public String getMailingState() {
-        return mailingState;
-    }
-
-    public void setMailingState(String mailingState) {
-        this.mailingState = mailingState;
-    }
-
-    public String getMailingZip() {
-        return mailingZip;
-    }
-
-    public void setMailingZip(String mailingZip) {
-        this.mailingZip = mailingZip;
-    }
-
-    public String getMailingCountry() {
-        return mailingCountry;
-    }
-
-    public void setMailingCountry(String mailingCountry) {
-        this.mailingCountry = mailingCountry;
-    }
+    private String salutation;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String suffix;
+    private String accountName;
+    private String title;
+    private String email;
+    private String phone;
+    private String mobile;
+    private String reportsTo;
+    private String department;
+    private String fax;
+    private String mailingStreet;
+    private String mailingCity;
+    private String mailingState;
+    private String mailingZip;
+    private String mailingCountry;
 }
--- src/test/java/tests/ContactTest.java
+++ src/test/java/tests/ContactTest.java
@@ -2,37 +2,36 @@ package tests;
 
 import models.Contact;
 import org.testng.annotations.Test;
-
 import java.util.Date;
 
 public class ContactTest extends BaseTest {
     Date currentDate = new Date();
-    Contact contact = new Contact(
-            "Mr.",
-            "FirstTest1",
-            "MiddleTest1",
-            "Contact" + currentDate,
-            "Suf",
-            "Test1",
-            "Test Title",
-            "test@gmail.com",
-            "9293336666",
-            "9293336666",
-            "HeadContact",
-            "QA",
-            "9293336666",
-            "Test Street",
-            "Test City",
-            "FL",
-            "33333",
-            "USA"
-    );
+    Contact contact = Contact.builder()
+            .salutation("Mr.")
+            .firstName("FirstTest1")
+            .middleName("MiddleTest1")
+            .lastName("Contact" + currentDate)
+            .suffix("Suf")
+            .accountName("Test1")
+            .title("Test Title")
+            .email("test@gmail.com")
+            .phone("929333666")
+            .mobile("929333666")
+            .reportsTo("HeadContact")
+            .department("Test Department")
+            .fax("929333666")
+            .mailingStreet("Test Street")
+            .mailingCity("TestCity")
+            .mailingState("FL")
+            .mailingZip("33666")
+            .mailingCountry("USA")
+            .build();
 
     @Test
     public void createAndRemoveContact() {
         loginSteps.open(login_url);
         loginSteps.login(username, password);
         contactSteps.createNewContact(contact);
-        //contactSteps.removeAccount(contact.getLastName());
+        contactSteps.removeAccount(contact.getLastName());
     }
 }

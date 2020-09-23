0afeb84fd713ff88e0586c3da340141e2a7bb144
Artur Dvorak <artur.dvorak@gmail.com>
2020-09-23 00:12:48 -0400

    TestAPI updates

--- src/test/java/tests/AccountTestAPI.java
+++ src/test/java/tests/AccountTestAPI.java
@@ -10,13 +10,12 @@ public class AccountTestAPI extends BaseTest {
 
     @Test
     public void createAccountUsingAPI() {
-        System.out.println(getAccessToken());
           Response response = given().auth().oauth2(getAccessToken()).header(HTTP.CONTENT_TYPE, ContentType.JSON)
                 .body("{\"name\": \"Test100\", \"description\": \"Test100 description\"}")
                 .when()
-                .post("https://ap16.lightning.force.com/services/data//v49.0/sobject/Account")
+                .post("https://ap16.salesforce.com/services/data/v49.0/sobjects/account/")
                 .then()
-                //.statusCode(201)
+                .statusCode(201)
                 .extract().response();
         System.out.println(response.body().asString());
     }

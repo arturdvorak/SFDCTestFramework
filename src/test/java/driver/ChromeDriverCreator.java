ae6f9c09caa599a183ea88379c5bce540c414632
Artur Dvorak <artur.dvorak@gmail.com>
2020-11-03 16:04:03 -0500

    Add default path to Chromedriver at CircleCI

--- src/test/java/driver/ChromeDriverCreator.java
+++ src/test/java/driver/ChromeDriverCreator.java
@@ -3,6 +3,7 @@ package driver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import static java.lang.System.setProperty;
 
 public class ChromeDriverCreator extends WebDriverCreator {
     private static final String CHROME_PROPERTY = "webdriver.chrome.driver";
@@ -10,9 +11,17 @@ public class ChromeDriverCreator extends WebDriverCreator {
     //private static final String driverPath = "src/test/resources/";
     private static final String driverPath = "src/test/resources/linux/";
     private ChromeOptions options;
+    private static String osName = System.getProperty("os.name").toLowerCase();
 
     public WebDriver factoryMethod() {
-        System.setProperty(CHROME_PROPERTY, driverPath+CHROME_DRIVER);
+        if (osName.contains("win")) {
+            setProperty(CHROME_PROPERTY, driverPath + "/chromedriver.exe");
+        } else if (osName.contains("mac")) {
+            setProperty(CHROME_PROPERTY, driverPath + "/chromedriver");
+        } else {
+            setProperty(CHROME_PROPERTY, driverPath + "/linux/chromedriver");
+        }
+        //setProperty(CHROME_PROPERTY, driverPath+CHROME_DRIVER);
         return new ChromeDriver(getChromeOptions());
     }
 

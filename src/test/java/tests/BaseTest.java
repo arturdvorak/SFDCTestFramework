b8c4954fa8eae13ea2baf9d86d42c177404bc0ff
Artur Dvorak <artur.dvorak@gmail.com>
2020-11-05 13:57:57 -0500

    TestListener adding + USERNAME and PASSWORD env variables names updating

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -3,14 +3,17 @@ package tests;
 import driver.BrowserType;
 import driver.WebDriverSingleton;
 import org.openqa.selenium.WebDriver;
+import org.testng.ITestContext;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.Listeners;
 import steps.AccountSteps;
 import steps.ContactSteps;
 import steps.LoginSteps;
 import org.testng.annotations.BeforeClass;
 import utils.PropertyReader;
-import java.io.IOException;
+import utils.TestListener;
 
+@Listeners(TestListener.class)
 public abstract class BaseTest {
     public static String browserType;
     public static String loginUrl;
@@ -24,8 +27,8 @@ public abstract class BaseTest {
     @BeforeClass(description = "Initializing properties and browser")
     public void setUp() {
         loginUrl = System.getenv().getOrDefault("LOGIN_URL", PropertyReader.getProperty("login.url"));
-        username = System.getenv().getOrDefault("USERNAME", PropertyReader.getProperty("username"));
-        password = System.getenv().getOrDefault("PASSWORD", PropertyReader.getProperty("password"));
+        username = System.getenv().getOrDefault("SF_USERNAME", PropertyReader.getProperty("username"));
+        password = System.getenv().getOrDefault("SF_PASSWORD", PropertyReader.getProperty("password"));
         browserType = System.getenv().getOrDefault("BROWSER_TYPE", PropertyReader.getProperty("browser.type"));
         driver = WebDriverSingleton.getWebDriverInstance(BrowserType.valueOf(browserType));
         loginSteps = new LoginSteps(driver);

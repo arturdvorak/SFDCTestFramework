d3ced8c86a86a993f98e26294ed9864dc9fc1943
Artur Dvorak <artur.dvorak@gmail.com>
2020-11-05 16:31:29 -0500

    Setting driver into context

--- src/test/java/utils/RetryAnalyzer.java
+++ src/test/java/utils/RetryAnalyzer.java
@@ -4,20 +4,15 @@ import org.testng.IRetryAnalyzer;
 import org.testng.ITestResult;
 
 public class RetryAnalyzer implements IRetryAnalyzer {
-    int counter = 0;
+    private static final int RETRY_LIMIT = 4;
+    private int counter = 0;
 
     @Override
     public boolean retry(ITestResult result) {
-        // check if the test method had RetryCountIfFailed annotation
-        RetryCountIfFailed annotation = result.getMethod().getConstructorOrMethod().getMethod()
-                .getAnnotation(RetryCountIfFailed.class);
-        // based on the value of annotation see if test needs to be rerun
-        if((annotation != null) && (counter < annotation.value()))
-        {
+        if(counter < RETRY_LIMIT) {
             counter++;
             return true;
         }
         return false;
     }
-
 }

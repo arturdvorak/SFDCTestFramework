bc816cd2e1e3742341d945ae6f29d93d1d6bb2c6
Artur Dvorak <artur.dvorak@gmail.com>
2020-11-10 01:34:18 -0500

    adding logs accross Framework Delete account/contact fixing

--- src/test/java/elements/BaseElement.java
+++ src/test/java/elements/BaseElement.java
@@ -1,10 +1,8 @@
 package elements;
 
-import lombok.extern.log4j.Log4j2;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-@Log4j2
 public class BaseElement {
     protected WebDriver driver;
     protected String title;
